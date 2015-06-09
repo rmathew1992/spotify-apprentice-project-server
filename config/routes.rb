33bf107a9fa4b8e46378dc7f7adf4f81401b2924
Helen Hood <helen.c.hood@gmail.com>
2015-06-09 14:05:05 -0400

    Add versionist & artists index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Rails.application.routes.draw do
+  api_version(module: 'V1',
+              header: {
+                name: 'Accept',
+                value: 'application/vnd.spotify-apprentice-app.com; version=1'
+              },
+              defaults: { format: :json }) do
+    resources :artists, only: :index
+  end
 end
