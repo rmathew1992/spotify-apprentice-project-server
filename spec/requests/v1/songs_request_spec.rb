f0286fb1c38da8844b0e08a6e675d204a6b14a98
rmathew1992 <rachel@Rachels-MacBook-Pro.local>
2015-06-10 14:10:54 -0400

    Putting the tests in a sensible place

--- spec/requests/v1/songs_request_spec.rb
+++ spec/requests/v1/songs_request_spec.rb
@@ -11,3 +11,19 @@ describe 'songs endpoints' do
     end
   end
 end
+
+describe 'song endpoint' do
+  describe 'GET /song' do
+    it 'goes to the correct endpoint' do
+      assert_generates '/songs/1', { controller: 'v1/songs', action: 'show', id: '1' }
+    end
+
+    it 'returns JSON for a single song' do
+      song = create(:song)
+
+      get(song_url(song), {}, accept_headers)
+      expect(response).to have_http_status :ok
+      expect(response).to match_response_schema :song
+    end
+  end
+end
