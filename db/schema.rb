bda0696b50f0a06c51d1697789134aca41edcd52
rmathew1992 <rachel@Rachels-MacBook-Pro.local>
2015-06-10 15:38:48 -0400

    Added an association for artists in the song table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609193512) do
+ActiveRecord::Schema.define(version: 20150610192900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,10 +47,12 @@ ActiveRecord::Schema.define(version: 20150609193512) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "name"
-    t.string   "artist"
     t.string   "spotify_uri"
+    t.uuid     "artist_id"
   end
 
+  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   add_index "songs", ["spotify_uri"], name: "index_songs_on_spotify_uri", unique: true, using: :btree
 
+  add_foreign_key "songs", "artists"
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,11 +2,9 @@ require 'rails_helper'
 
 describe Song do |variable|
   describe 'validations' do
-    it { should validate_presence_of :name}
-    it {should validate_presence_of :artist}
+    it {should validate_presence_of :name}
     it {should validate_presence_of :artist_id}
     it {should validate_presence_of :spotify_uri}
     it {should validate_uniqueness_of :spotify_uri }
   end
 end
-
