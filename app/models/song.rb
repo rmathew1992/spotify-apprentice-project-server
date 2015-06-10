311a3fe2509c5ddca544c1a265c9a358946501d4
rmathew1992 <rachel@Rachels-MacBook-Pro.local>
2015-06-10 17:15:24 -0400

    Adding relation

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   validates :name, presence: true
   validates :spotify_uri, presence: true, uniqueness: true
-  validates :artist_id, presence: true
+
+  belongs_to :artist
 end
