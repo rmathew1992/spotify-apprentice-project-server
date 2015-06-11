45608b1ec2938649817ee49ea022f8b2e6b0ca9f
rmathew1992 <rachel@Rachels-MacBook-Pro.local>
2015-06-11 07:41:08 -0400

    Change tests to expect multiple songs

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Artist do
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :song_ids}
     it { should validate_presence_of :spotify_uri }
     it { should validate_uniqueness_of :spotify_uri }
   end
