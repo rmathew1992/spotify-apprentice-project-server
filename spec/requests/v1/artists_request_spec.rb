7bbaf18664d3bd18bc336c9e08cea29508c850b9
rmathew1992 <rachel@Rachels-MacBook-Pro.local>
2015-06-11 09:34:30 -0400

    Modifying tests to expect a has_many relation with songs in artist specs, and removed an extra binding.pry

--- spec/requests/v1/artists_request_spec.rb
+++ spec/requests/v1/artists_request_spec.rb
@@ -6,7 +6,6 @@ describe 'artists endpoints' do
       artists = create_list(:artist, 1)
 
       get(artists_url, {}, accept_headers)
-binding.pry
       expect(response).to have_http_status :ok
       expect(response).to match_response_schema :artists
     end
