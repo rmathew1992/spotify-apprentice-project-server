6a14c19ab52a0753f82ceef1dd5057960979ac0c
rmathew1992 <rachel@Rachels-MacBook-Pro.local>
2015-06-11 08:29:08 -0400

    Make sure that all artists have songs

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ActiveRecord::Base
   validates :name, presence: true
   validates :spotify_uri, presence: true, uniqueness: true
+  validates :songs, presence:true
 
   has_many :songs
 end
