1c3846510ee5069968ec0ca5a4ca5469d3445e97
rmathew1992 <rachel@Rachels-MacBook-Pro.local>
2015-06-12 10:22:17 -0400

    Getting GET /artists happy path test to pass

--- app/decorator/song_decorator.rb
+++ app/decorator/song_decorator.rb
@@ -1,7 +1,7 @@
 class SongDecorator < Draper::Decorator
   def popularity
     if object.plays > 3000
-      "#{object.name} has #{object.plays}, it is popular"
+      "#{object.name} has #{object.plays} plays, it is popular"
     end
   end
 end
