c401ce5ff1186cfa0aa45838ba242a24be43230e
rmathew1992 <rachelmathew999@gmail.com>
2015-06-16 14:40:33 -0400

    Serialized mixtapes using the song serializer

--- app/controllers/v1/mixtapes_controller.rb
+++ app/controllers/v1/mixtapes_controller.rb
@@ -5,7 +5,6 @@ class V1::MixtapesController < ApplicationController
 
   def create
     mixtape = MixtapeMaker.generate(params[:number_of_songs], artist_ids: params[:artist_ids])
-    render json: mixtape
+    render json: mixtape, each_serializer: SongSerializer, root: 'mixtape', include: [:artist]
   end
-
 end
--- spec/requests/v1/mixtape_request_spec.rb
+++ spec/requests/v1/mixtape_request_spec.rb
@@ -7,7 +7,6 @@ describe 'mixtape endpoints' do
       songs = create_list(:song, 5, artist: artist)
 
       post(mixtapes_url, { number_of_songs: 4, artist_ids: artist[:id] }.to_json, accept_headers)
-      binding.pry
       expect(response).to have_http_status :ok
       expect(response).to match_response_schema :mixtape
     end
